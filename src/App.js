9aedc2595b8322a54a760a1a2b22309835dfa631
Jon Haase <haase1020@gmail.com>
2020-08-09 08:01:54 -0400

    laying groundwork for calendary component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Calendar from './components/calendar/calendar';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Route exact path='/calendar' component={Calendar} />
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <p>Welcome to your calendar~</p>
+
+        <Route exact path='/calendar' component={Calendar} />
+      </Fragment>
+    </Router>
   );
 }
 
